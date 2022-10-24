9732dc46d9561f9d6b2b8f59192d599dba479eda
Mylan Connolly <mylan@mylan.io>
2022-10-24 15:08:49 -0400

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,3 +1,5 @@
-run 'bin/rails action_mailbox:install'
-run 'bin/rails active_storage:install'
-run 'bin/rails action_text:install
+after_bundle do
+  run 'bin/rails action_mailbox:install'
+  run 'bin/rails active_storage:install'
+  run 'bin/rails action_text:install'
+end
